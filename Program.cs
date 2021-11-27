30ba1e18d07fc13f7a963d2b0021309e156f6efb
Lucas Mota <lucasmota7@gmail.com>
2021-11-27 12:55:06 -0300

    Exercicio usando While

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio03While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int cont = 0;
+
+            while (cont <= 100)
+            {
+
+                Console.WriteLine("Contagem de numeros pares: " + cont);
+
+                cont += 2;
+            }
+
+            Console.ReadKey();
+
+        }
+
+        
+
+        
+    }
+}
